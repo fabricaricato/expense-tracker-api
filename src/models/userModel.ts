25e99b94fdc6716fefa43c435913bbbdccdaf57a
Fabrizio <fabriziocaricato1@gmail.com>
2026-07-03 14:37:50 -0300

    feat: add user model

---
+++ src/models/userModel.ts
@@ -0,0 +1,28 @@
+import { model, Schema } from "mongoose"
+import { IUser } from "../interfaces/IUser"
+
+const UserSchema = new Schema<IUser>({
+  username: {
+    type: String,
+    default: "New User"   
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true          
+  },
+  password: {
+    type: String,
+    required: true        
+  }
+},
+  {
+    versionKey: false,  
+    timestamps: true
+  },
+  
+)
+
+const User = model<IUser>('User', UserSchema)
+
+export { User }
